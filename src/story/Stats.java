0ad581b81acc9ebf943a6e310703b4485a605a3c
Furi <129733615+furi-tesla@users.noreply.github.com>
2023-04-19 22:33:45 -0500

    mini story gen in progress

--- src/story/Stats.java
+++ src/story/Stats.java
@@ -1,4 +1,22 @@
 package story;
 
 public class Stats extends Personality {
+    private int heightInFt;
+    private int heightInInches;
+    private double weightInLbs;
+    private int intelligence;
+    private int wisdom;
+    private int dexterity;
+    private int strength;
+    private int constitution;
+    private int charisma;
+    private String intQualifier;
+    private String wisQualifier;
+    private String dexQualifier;
+    private String strQualifier;
+    private String conQualifier;
+    private String chaQualifier;
+
+
+
 }
